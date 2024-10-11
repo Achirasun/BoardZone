f3c9fb2faf3d6f7a2f0493b38fac80c6196d8229
66070185 <66070185@kmitl.ac.th>
2024-10-11 22:01:17 +0700

    Minor: Fix App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { NavBar } from './components/NavBar/NavBar';
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import { Home, BoardGame } from './components/pages';
 
 function App() {
   return (
-    <div className="container">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/boardgame" element={<BoardGame />} />
-      </Routes>
+    <div>
+
+      <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/boardgame" element={<BoardGame />} />
+        </Routes>
+      </div>
     </div>
   );
 }
