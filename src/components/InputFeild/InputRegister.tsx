5e3ce3b4bfb770cf564868216c7179eb767e3a19
worawich choonpicharn <163491348+id66070180@users.noreply.github.com>
2024-10-12 16:28:38 +0700

    Add and Update Input login
    Add Input for Register
    Add error check
    Add error warning
    Update Styles.css to look better

--- src/components/InputFeild/InputRegister.tsx
+++ src/components/InputFeild/InputRegister.tsx
@@ -4,18 +4,23 @@ import { MdOutlinePassword } from "react-icons/md";
 import './styles.css';
 import { Link, useNavigate } from 'react-router-dom';
 
-const InputUsername: React.FC = () => {
+const InputRegister: React.FC = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmpassword, setconfirmPassword] = useState('');
   const [error, setError] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    if (!username || !password) {
+    if (!username || !password || !confirmpassword) {
       setError('Username and Password cannot be empty');
       return;
-    }
+    } else
+    if (password != confirmpassword) {
+        setError('Password and Confirm Password\nmust be the same');
+        return;
+      }
 
     setError('');
     console.log('Username:', username);
@@ -50,12 +55,24 @@ const InputUsername: React.FC = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
+      <div className="form-group">
+        <div className="avatar-container">
+          <MdOutlinePassword color='#4E4E4E' />
+        </div>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          className="input__box"
+          value={confirmpassword}
+          onChange={(e) => setconfirmPassword(e.target.value)}
+        />
+      </div>
       {error && <p className='error-message'>{error}</p>}
       <div className="form-group">
-          <button type='submit' className='input__submit'>Log in</button>
+          <button type='submit' className='input__submit'>Sign Up</button>
       </div>
     </form>
   );
 };
 
-export default InputUsername;
+export default InputRegister;
