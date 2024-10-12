afe9519500d2ef7c21a91b1510c703c118a64ef7
Pusrik <138851131+Pusrik@users.noreply.github.com>
2024-10-12 18:31:14 +0700

    Funds Page

---
+++ src/components/pages/Funds.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Funds.css'
+import FundList from '../FundsList/FundList'
+
+export const Funds = () => {
+    return (
+        <div>
+        <FundList />
+        </div>
+    )
+}
+
+export default Funds;
--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,5 +1,3 @@
 export * from "./Home"
 export * from "./BoardGame"
-export * from "./Login"
-export * from "./Register"
-export * from "./Lobby"
+export * from "./Funds"
